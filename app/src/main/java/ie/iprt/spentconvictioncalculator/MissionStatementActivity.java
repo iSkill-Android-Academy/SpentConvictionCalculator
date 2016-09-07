3b6ceedee214472e3eaf7e60bf61da99350cd772
Adam <afairmanking@gmail.com>
2016-09-07 09:49:29 +0100

    initial commit

---
+++ app/src/main/java/ie/iprt/spentconvictioncalculator/MissionStatementActivity.java
@@ -0,0 +1,13 @@
+package ie.iprt.spentconvictioncalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MissionStatementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission_statement);
+    }
+}
