927b9ff84a45016a3469caa13cc7e48a64fc88cd
joshcarroll93 <93.joshuacarroll@gmail.com>
2016-09-09 14:58:13 +0100

    josh updated fonts and view

--- app/src/main/java/ie/iprt/spentconvictioncalculator/AboutActivity.java
+++ app/src/main/java/ie/iprt/spentconvictioncalculator/AboutActivity.java
@@ -1,17 +1,30 @@
 package ie.iprt.spentconvictioncalculator;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private TextView mAbout;
+    private TextView mAboutDevelopers;
+    private TextView mAboutIskill;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/american_typewriter_font.ttf");
+        mAbout = (TextView)findViewById(R.id.about_text_id_font);
+        mAbout.setTypeface(tf);
+        mAboutDevelopers = (TextView)findViewById(R.id.about_developers_id_font);
+        mAboutDevelopers.setTypeface(tf);
+        mAboutIskill = (TextView)findViewById(R.id.about_iskill_id_font);
+        mAboutIskill.setTypeface(tf);
     }
     public void onDonateClick(View View){
         goToUrl("https://www.iprt.ie/donation-form");
