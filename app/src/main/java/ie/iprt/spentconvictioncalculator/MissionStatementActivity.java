8b4c79ac84a509f337418af2a1c7ff48c58e3bde
oluwaseunonasanwo <christinaoxygen@gmail.com>
2016-09-09 13:23:17 +0100

    added instuctions font

--- app/src/main/java/ie/iprt/spentconvictioncalculator/MissionStatementActivity.java
+++ app/src/main/java/ie/iprt/spentconvictioncalculator/MissionStatementActivity.java
@@ -1,9 +1,11 @@
 package ie.iprt.spentconvictioncalculator;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MissionStatementActivity extends AppCompatActivity {
 
@@ -11,6 +13,10 @@ public class MissionStatementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission_statement);
+//casting the font to the text view
+        TextView instructions_header= (TextView) findViewById(R.id.instructions_header);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/american_typewriter_font.ttf");
+        instructions_header.setTypeface(tf);
     }
 
     public void FormActivity (View view){
